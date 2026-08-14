28ebad17fdbd5758d3214c4e5cee19aa30d36877
Pedro Nunes <154082362+pedrorivanunes@users.noreply.github.com>
2026-08-14 02:54:45 -0300

    08-14-2026 0009-palindrome-number Math Easy 4 ms (100%) 45.8 MB (87.57%) .java

---
+++ Java/Easy/0009-palindrome-number/0009-palindrome-number-08-14-2026-02-54-45.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if(x < 0) return false;
+        if(x != 0 && x % 10 == 0) return false;
+
+        int inverted = 0;
+        
+        while(x > inverted) {
+            int digit = x % 10;
+            inverted = inverted * 10 + digit;
+            x = x / 10;
+        }
+
+        return x == inverted || x == inverted / 10;
+    }
+}
