fe384aefe32df7d8f6342a1b55e7e6d4bc669497
Pedro Nunes <154082362+pedrorivanunes@users.noreply.github.com>
2026-08-19 23:20:13 -0300

    08-19-2026 0238-product-of-array-except-self Array Medium 2 ms (93.74%) 71.4 MB (64.77%)

---
+++ Java/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self-08-19-2026-23-20-12.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] answer = new int[nums.length];
+        answer[0] = 1;
+        int rightProduct = 1;
+        for(int i = 1; i < nums.length; i++) {
+            answer[i] = nums[i - 1] * answer[i - 1];
+        }
+        for(int i = nums.length - 1; i >= 0; i--) {
+            answer[i] = answer[i] * rightProduct;
+            rightProduct = rightProduct * nums[i];
+        }
+        return answer;
+    }
+}
